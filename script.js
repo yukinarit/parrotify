a6f33746d2c8617737f19e34a8a3167eb821790c
Yukinari Tani <yukinarit84@gmail.com>
2020-07-07 13:44:25 +0900

    Replace :parrot: with parrot gif

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ console.debug('script.js is running now.');
 chrome.storage.sync.get('color', (data) => {
   const parrots = $(document).xpath("//*[text()[contains(.,':parrot:')]]");
   console.debug('Fetched by xpath:', parrots);
-  for (parrot of parrots) {
-    console.debug(`Text: ${parrot.innerText}`)
+  for (elm of parrots) {
+    // TODO Needs to ignore TextArea.
+    console.debug('[before] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
+
+    newHTML = elm.innerHTML.replace(/:parrot:/g, '<img src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif" style="width: 15px">');
+    elm.innerHTML = newHTML;
+
+    console.debug('[after] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
+    /*
     const img = document.createElement('img');
 
     img.src = 'https://cultofthepartyparrot.com/parrots/hd/parrot.gif';
     img.style.width = '15px';
-    parrot.innerText = '';
-    parrot.appendChild(img);
+    elm.innerText = '';
+    elm.appendChild(img);
+    */
   }
 
 });
