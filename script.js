f6f1b78cfca1072d2604b9fa6208767618e48a6e
Yukinari Tani <yukinarit84@gmail.com>
2020-07-07 21:44:20 +0900

    Add size table to determine size per tag

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 console.debug('script.js is running now.');
 
 chrome.storage.sync.get('color', (data) => {
-  const parrots = $(document).xpath("//*[text()[contains(.,':parrot:')]]");
-  console.debug('Fetched by xpath:', parrots);
-  for (elm of parrots) {
+  emojiSizes = {
+    h1: 32,
+    h2: 24,
+  };
+  const elms = $(document).xpath("//*[text()[contains(.,':parrot:')]]");
+  console.debug('Fetched by xpath:', elms);
+  for (elm of elms) {
     // TODO Needs to ignore TextArea.
     console.debug('[before] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
+    const tag = elm.tagName.toLowerCase();
+    const width = emojiSizes[tag] || 15;
+    console.debug('tagName: ', tag, ', width: ', width);
 
-    newHTML = elm.innerHTML.replace(/:parrot:/g, '<img src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif" style="width: 15px">');
+    newHTML = elm.innerHTML.replace(/:parrot:/g, `<img src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif" style="width: ${width}px">`);
     elm.innerHTML = newHTML;
 
     console.debug('[after] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
-    /*
-    const img = document.createElement('img');
-
-    img.src = 'https://cultofthepartyparrot.com/parrots/hd/parrot.gif';
-    img.style.width = '15px';
-    elm.innerText = '';
-    elm.appendChild(img);
-    */
   }
-
 });
