60ec82dda0f45ec0ac257a07f1619c26d94e685b
Yukinari Tani <yukinarit84@gmail.com>
2020-07-09 21:44:40 +0900

    feat: Filter by tag name

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-console.debug('script.js is running now.');
+const parrot = 'https://cultofthepartyparrot.com/parrots/hd/parrot.gif';
+console.info('%c.', `font-size: 1px; line-height: 70px; padding: 30px 60px; background: url("${parrot}");`);
 
-chrome.storage.sync.get('color', (data) => {
-  emojiSizes = {
+const EmojiSizes = {
     h1: 32,
     h2: 24,
-  };
+    h3: 20,
+    default_value: 15,
+};
+
+/*
+ * Return optimal emoji sizes for a given tag.
+ */
+class EmojiSize {
+  static get(tag) {
+    return EmojiSizes[tag] || EmojiSizes['default_value'];
+  }
+}
+
+const IGNORE_TAGS = [
+  'textarea', 'pre', 'code',
+];
+
+function* filter_tag(elms) {
+  for (elm of elms) {
+    if (!IGNORE_TAGS.includes(elm.tagName.toLowerCase())) {
+      yield elm;
+    }
+  }
+}
+
+$(() => {
   const elms = $(document).xpath("//*[text()[contains(.,':parrot:')]]");
   console.debug('Fetched by xpath:', elms);
-  for (elm of elms) {
-    // TODO Needs to ignore TextArea.
+
+  for (elm of filter_tag(elms)) {
     console.debug('[before] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
     const tag = elm.tagName.toLowerCase();
-    const width = emojiSizes[tag] || 15;
+    const width = EmojiSize.get(tag);
     console.debug('tagName: ', tag, ', width: ', width);
 
-    newHTML = elm.innerHTML.replace(/:parrot:/g, `<img src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif" style="width: ${width}px">`);
+    newHTML = elm.innerHTML.replace(/:parrot:/g, `<img src="${parrot}" style="width: ${width}px">`);
     elm.innerHTML = newHTML;
 
     console.debug('[after] innerText:', elm.innerText, 'innerHTML', elm.innerHTML);
